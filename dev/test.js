56163815c986525b5d3b012052d403364ed03554
Abdullahi <abdullahiadamu600@gmail.com>
2021-05-29 19:33:16 +0100

    creating genesis blockchain

--- dev/test.js
+++ dev/test.js
@@ -2,25 +2,4 @@ const Blockchain = require('./blockchain');
 
 const bitcoin = new Blockchain();
 
-const previousBlockHash = 'hjsdhfjkdhsfudshi';
-const currentBlockData = [
-    {
-        amount: 10,
-        sender: 'bhjsdhbhkdsuig',
-        recipient: 'jkahsfjdsah8'
-    },
-    {
-        amount: 30,
-        sender: 'ghjkhgjk',
-        recipient: 'gfhjkkkjl'
-    },
-    {
-        amount: 40,
-        sender: 'ghjkl',
-        recipient: 'hgbjnmjgfhjk'
-    }
-];
-
-const nonce = 100;
-
-console.log(bitcoin.hashBlock(previousBlockHash, currentBlockData, 180786));
+console.log(bitcoin);
